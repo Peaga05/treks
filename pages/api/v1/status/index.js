cdde283d875182a726f123f12380bab1012e3ce4
Paulo Henrique <phserafim2020@gmail.com>
2026-01-12 21:22:20 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,10 @@
+import database from "../../../../infra/database.js"
 //Request: o que é mandando na requisição
 //Response: retorno da requisição
 
-function status(request, response) {
+async function status(request, response) {
+  var result = await database.query("SELECT 1+1 as SUM;");
+  console.log(result.rows);
   response.status(200).json({ resposta: "Teste da api" });
 }
 
