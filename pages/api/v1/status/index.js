cbae3a585f6b549a973750ff6b223d0e805addf0
Paulo Henrique <phserafim2020@gmail.com>
2026-01-17 16:23:45 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database.js"
+
 //Request: o que é mandando na requisição
 //Response: retorno da requisição
 
 async function status(request, response) {
-  var result = await database.query("SELECT 1+1 as SUM;");
-  console.log(result.rows);
-  response.status(200).json({ resposta: "Teste da api" });
+  const updateAt = new Date().toISOString();
+
+  const postgresVersionQuery = await database.query("SHOW server_version;")
+  const postgresVersion = postgresVersionQuery.rows[0].server_version;;
+
+  const maxConnectionQuery = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionQuery.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnectionQuery = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+
+  const openConnection = usedConnectionQuery.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: parseInt(maxConnections),
+        open_connection: openConnection,
+      }
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json(); //Pega o body da resposta e transforma em um Json
+  expect(responseBody.update_at).toBeDefined();
+
+  const parseUPdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parseUPdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.11")
+  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  expect(parseInt(responseBody.dependencies.database.open_connection)).toBe(1)
 });
