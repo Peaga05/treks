6ea95c45b07e26ac748f5c5d800e5e484862b5d4
Paulo Henrique <phserafim2020@gmail.com>
2026-03-12 20:40:26 -0300

    fix: chnage `join` to `resolve` in `api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
@@ -17,7 +17,7 @@ export default async function migrations(request, response) {
     const defaultMigrationOptions = {
       dbClient: dbClient, //Instancia de conexão com o banco de dados
       dryRun: true, //Roda no modo dry - apenas retornar as migrations que rodariam
-      dir: join("infra", "migrations"), // caminho de onde está as migrations
+      dir: resolve("infra", "migrations"), // caminho de onde está as migrations
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
